ca1b6c77051b4ba76f16f17b7e926cdb25fd5973
takagi <iam.takagi.dev@gmail.com>
2022-02-15 17:05:19 +0900

    Fix: url (ytwp.iamtakagi.net)

--- public/script.js
+++ public/script.js
@@ -3,5 +3,5 @@ const playButton = document.getElementById("play");
 const player = document.getElementById("player");
 
 playButton.addEventListener("click", () => {
-  player.src = `http://localhost:3030/stream?url=${urlInput.value}`
+  player.src = `https://ytwp.iamtakagi.net/stream?url=${urlInput.value}`
 });
