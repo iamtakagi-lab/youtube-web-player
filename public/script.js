014ee744e05118e66d2bd6097148f22dbaf75c58
takagi <iam.takagi.dev@gmail.com>
2022-02-16 00:21:11 +0900

    Cleanup

--- public/script.js
+++ public/script.js
@@ -1,9 +1,9 @@
 const urlInput = document.getElementById("url");
 const playButton = document.getElementById("play");
 const player = document.getElementById("player");
+const qualitySelectbox = document.getElementById("quality")
 
 const play = () => {
-  const qualitySelectbox = document.getElementById("quality")
   const url = urlInput.value
   const quality = qualitySelectbox.value
   const streamUrl = `https://ytwp.iamtakagi.net/stream?url=${url}&quality=${quality}`
