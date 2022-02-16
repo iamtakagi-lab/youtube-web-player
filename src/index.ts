faf6d9dbc5b9eddb600f923ece3f0901a1ce9ec0
takagi <iam.takagi.dev@gmail.com>
2022-02-17 03:16:59 +0900

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ app.get("/stream", async (req, res, next) => {
   const audioStream = ytdl.downloadFromInfo(info, { quality: 'highestaudio' });
   const videoStream = ytdl.downloadFromInfo(info, { quality: 'highestvideo' });
   //映像と音声を結合するエンコード処理
-  const ffmpegProcess = spawn("ffmpeg", [
+  const ffmpeg = spawn("ffmpeg", [
       '-loglevel', '8', '-hide_banner',
       '-i', 'pipe:3', '-i', 'pipe:4',
       '-map', '0:a', '-map', '1:v',
@@ -27,13 +27,16 @@ app.get("/stream", async (req, res, next) => {
       ]
   }) as any
   //用意したパイプに流し込む
-  audioStream.pipe(ffmpegProcess.stdio[3]);
-  videoStream.pipe(ffmpegProcess.stdio[4]);
+  audioStream.pipe(ffmpeg.stdio[3]);
+  videoStream.pipe(ffmpeg.stdio[4]);
   //エンコード結果をストリーム化
-  const resultStream = ffmpegProcess.stdio[5].pipe(new stream.PassThrough({highWaterMark: 1920 * 1080}))
+  const resultStream = ffmpeg.stdio[5].pipe(new stream.PassThrough({highWaterMark: 1920 * 1080}))
   res.contentType("video/mp4");
   res.attachment(`${info.videoDetails.title}.mp4`);
   resultStream.pipe(res, {end: true})
+  res.on("close",function(){
+    ffmpeg.kill();
+  })
 });
 
 app.use(express.static(path.resolve(__dirname, "..", "public")));
