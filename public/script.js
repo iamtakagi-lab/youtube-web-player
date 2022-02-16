6f732cba55c8a9c4a13298823585e318e5fdfaa6
takagi <iam.takagi.dev@gmail.com>
2022-02-17 02:31:16 +0900

    Remove: quality

--- public/script.js
+++ public/script.js
@@ -1,12 +1,10 @@
 const urlInput = document.getElementById("url");
 const playButton = document.getElementById("play");
 const player = document.getElementById("player");
-const qualitySelectbox = document.getElementById("quality")
 
 const play = () => {
   const url = urlInput.value
-  const quality = qualitySelectbox.value
-  const streamUrl = `https://ytwp.iamtakagi.net/stream?url=${url}&quality=${quality}`
+  const streamUrl = `https://ytwp.iamtakagi.net/stream?url=${url}`
   player.src = streamUrl
 }
 
